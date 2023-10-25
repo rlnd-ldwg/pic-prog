2ed59b65ca20d388fd8132d0020ffb1b37afc3ff
rlnd ldwg <zkh.60438@gmail.com>
2023-10-25 21:15:28 +0200

    Command file, include path for pio

---
+++ inc/commands.h
@@ -0,0 +1,33 @@
+/* defined comands */
+
+#define ENTER_PROGMODE          0x01
+#define EXIT_PROGMODE           0x02
+#define ISP_RESET_PTR           0x03
+#define ISP_SEND_CFG            0x04
+#define ISP_INC_PTR             0x05
+#define ISP_READ_PGM            0x06
+#define ISP_MASS_ERASE          0x07
+#define ISP_WRITE_PRGM          0x08
+#define ISP_RESET_PTR           0x09
+#define P18_ENTER_PROGMODE      0x10
+#define P18_ISP_READ_PGM        0x11
+#define P18_ISP_WRITE_PGM       0x12
+#define P18_ISP_MASS_ERASE      0x13
+#define P18_ISP_WRITE_CFG       0x14
+#define P18FJ_ISP_MASS_ERASE    0x23
+#define P18FK_ISP_MASS_ERASE    0x30
+#define P18FK_ISP_WRITE_PGM     0x31
+#define P18FK_ISP_WRITE_CFG     0x32
+#define P16C_ENTER_PROGMODE     0x40
+#define P16C_ISP_READ_PGM       0x41
+#define P16C_ISP_WRITE_PGM      0x42
+#define P16C_BULK_ERASE         0x43
+#define P16C_ISP_WRITE_CFG      0x44
+#define P18Q_ISP_WRITE_CFG      0x45
+#define P18Q_ISP_WRITE_PGM      0x46
+#define P18Q_ISP_READ_CFG       0x47
+#define P18Q_ISP_WRITE_CFG_ADDR 0x48
+#define P18QXX_BULK_ERASE       0x49
+#define ENTER_PROGMODE_HVP      0x91
+#define EXIT_PROGMODE_HVP       0x92
+
